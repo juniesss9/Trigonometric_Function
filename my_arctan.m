e3a7d2dbab67714661032877465646fe67cd18a2
juniesss9 <2353935637@qq.com>
2024-04-02 16:28:10 +0800

    arctan

--- my_arctan.m
+++ my_arctan.m
@@ -1,8 +1,8 @@
-function result = my_cos(x)
+function result = my_arctan(x)
     result = 0;
     n = 10; % 展开项数，可以根据需要调整
     for k = 0:n
-        result = result + ((-1)^k * x^(2*k)) / factorial(2*k);
+        result = result + ((-1)^k * x^(2*k+1)) / (2*k+1);
     end
 end
 
